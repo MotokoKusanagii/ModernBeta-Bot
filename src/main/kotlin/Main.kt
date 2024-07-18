1e49cd8b2215999b2cb1492bbe99e95611746b43
motoko <meikoprivat@icloud.com>
2024-07-18 22:37:10 +0200

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,8 @@
 import dev.kord.common.entity.InteractionType
 import dev.kord.core.Kord
 import dev.kord.core.behavior.interaction.respondEphemeral
-import dev.kord.core.behavior.interaction.respondPublic
 import dev.kord.core.entity.interaction.ChatInputCommandInteraction
 import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
-import dev.kord.core.event.interaction.InteractionCreateEvent
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
 import dev.kord.gateway.Intent
@@ -20,13 +18,13 @@ suspend fun cmdRegister(interaction: ChatInputCommandInteraction) {
 }
 suspend fun main() {
 
-    val kord = Kord("token");
+    val kord = Kord("token")
 
     kord.on<MessageCreateEvent> {
         // ignore other bots
-        if (message.author?.isBot == true) return@on;
+        if (message.author?.isBot == true) return@on
 
-        message.channel.createMessage(message.content);
+        message.channel.createMessage(message.content)
     }
 
     kord.on<ChatInputCommandInteractionCreateEvent> {
@@ -41,5 +39,5 @@ suspend fun main() {
     kord.login {
         @OptIn(PrivilegedIntent::class)
         intents += Intent.MessageContent
-    };
+    }
 }
